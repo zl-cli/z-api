5384c0393835bc2f81b4c599d7c83a98099651e3
Lird <839122419@qq.com>
2023-06-14 23:50:57 +0800

    update api

--- src/lib.rs
+++ src/lib.rs
@@ -10,22 +10,22 @@ use model::*;
 #[Api]
 pub trait ZApi {
     #[Post("/eapi/user/login")]
-    pub fn login(&self, email: String, password: String) -> Result<Value>;
+    fn login(&self, email: String, password: String) -> Result<Value>;
 
     #[Get("/eapi/user/profile")]
-    pub fn get_profile(&self) -> Result<Value>;
+    fn get_profile(&self) -> Result<Value>;
 
     #[Get("/eapi/book/recently")]
-    pub fn get_book_recently(&self) -> Result<Vec<Book>>;
+    fn get_book_recently(&self) -> Result<Vec<Book>>;
 
     #[Get("/eapi/book/recommended")]
-    pub fn get_book_recommended(&self) -> Result<Vec<Book>>;
+    fn get_book_recommended(&self) -> Result<Vec<Book>>;
 
     #[Get("/eapi/book/most-popular")]
-    pub fn get_book_most_popular(&self, switch_language: String) -> Result<Vec<Book>>;
+    fn get_book_most_popular(&self, switch_language: String) -> Result<Vec<Book>>;
 
-    #[Get("/eapi/book/{id}/{hash}/send-to-{type}")]
-    pub fn send_book_to_device(&self, id: i32, hash: i32, device:String) -> Result<()>;
+    #[Get("/eapi/book/{id}/{hash}/send-to-{device}")]
+    fn send_book_to_device(&self, id: i32, hash: i32, device:String) -> Result<()>;
 
     
 }
