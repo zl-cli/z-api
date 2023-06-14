93e829ad42f9050ffabffec67f208bdf83979c38
Lird <839122419@qq.com>
2023-06-14 23:40:12 +0800

    add apis

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,31 @@
-use serde::{Deserialize,Serialize};
-
-use z_api_def::{Api, Post, Get};
 use serde_json::{Result, Value};
+use z_api_def::{Api, Get, Post};
 
-#[derive(Debug,Serialize, Deserialize)]
-pub struct Book {
+pub mod model;
 
-}
+use model::*;
+
 
 /// The api definitions.
 #[Api]
-pub trait  ZApi{
-    // #[Post("/eapi/user/login")]
-    // fn login(&self, email: String, password: String) -> Result<Value>;
+pub trait ZApi {
+    #[Post("/eapi/user/login")]
+    fn login(&self, email: String, password: String) -> Result<Value>;
 
     #[Get("/eapi/user/profile")]
     fn get_profile(&self) -> Result<Value>;
 
     #[Get("/eapi/book/recently")]
     fn get_book_recently(&self) -> Result<Vec<Book>>;
+
+    #[Get("/eapi/book/recommended")]
+    fn get_book_recommended(&self) -> Result<Vec<Book>>;
+
+    #[Get("/eapi/book/most-popular")]
+    fn get_book_most_popular(&self, switch_language: String) -> Result<Vec<Book>>;
+
+    #[Get("/eapi/book/{id}/{hash}/send-to-{type}")]
+    fn send_book_to_device(&self, id: i32, hash: i32, device:String) -> Result<()>;
+
+    
 }
---
+++ src/model.rs
@@ -0,0 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Book {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Info {}
