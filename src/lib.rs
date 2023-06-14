ef4495f11aec06f2134d57f826cae8fe144c7bba
Lird <839122419@qq.com>
2023-06-15 00:43:54 +0800

    add more api

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,63 @@
 use serde_json::{Result, Value};
+use std::time::Instant;
 use z_api_def::{Api, Get, Post};
 
 pub mod model;
 
 use model::*;
 
-
 /// The api definitions.
 #[Api]
 pub trait ZApi {
+    /*
+     * User Post Api
+     */
     #[Post("/eapi/user/login")]
     fn login(&self, email: String, password: String) -> Result<Value>;
 
+    /*
+     * User Get Api
+     */
     #[Get("/eapi/user/profile")]
     fn get_profile(&self) -> Result<Value>;
 
+    #[Get("/eapi/user/book/recommended")]
+    fn get_user_book_recommended(&self) -> Result<Vec<Book>>;
 
     /*
      * Book Get Api
      */
-
     #[Get("/eapi/book/recently")]
     fn get_book_recently(&self) -> Result<Vec<Book>>;
-
-    #[Get("/eapi/book/recommended")]
-    fn get_book_recommended(&self) -> Result<Vec<Book>>;
 
     #[Get("/eapi/book/most-popular")]
     fn get_book_most_popular(&self, switch_language: String) -> Result<Vec<Book>>;
 
     #[Get("/eapi/book/{id}/{hash}/send-to-{device}")]
-    fn send_book_to_device(&self, id: i32, hash: i32, device:String) -> Result<()>;
+    fn send_book_to_device(&self, id: i32, hash: i32, device: String) -> Result<()>;
 
     #[Get("/eapi/book/{id}/{hash}")]
     fn get_book(&self, id: i32, hash: i32, switch_language: String) -> Result<Book>;
 
     #[Get("/eapi/book/{id}/{hash}/similar")]
     fn get_similar_books(&self, id: i32, hash: i32) -> Result<Vec<Book>>;
-    
+
+    #[Get("/eapi/book/{id}/{hash}/formats")]
+    fn get_book_formats(&self, id: i32, hash: i32) -> Result<Vec<Format>>;
+
+    /*
+     * Book Post Api
+     */
+    #[Post("/eapi/book/search")]
+    fn search_books(
+        &self,
+        message: String,
+        yearFrom: String,
+        yearTo: String,
+        language: String,
+        extensions: String,
+        e: String,
+        page: i32,
+        limit: i32,
+    ) -> Result<Vec<Book>>;
 }
--- src/model.rs
+++ src/model.rs
@@ -8,3 +8,6 @@ pub struct Book {}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Info {}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Format {}
