3c6d6d4dd4fe06f720cfbbce76accaa6fc719d71
Lird <839122419@qq.com>
2023-06-15 01:49:59 +0800

    update

--- src/model.rs
+++ src/model.rs
@@ -1,13 +1,78 @@
+use std::{collections::HashMap, hash::Hash};
+
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+// example:
+// "user": {
+//     "id": 13416985,
+//     "email": "9211080n0225@njust.edu.cn",
+//     "name": "LRD",
+//     "kindle_email": "",
+//     "remix_userkey": "7470fa8c59efc4d3fe0a8b439de0f90d",
+//     "donations_active": null,
+//     "donations_expire": null,
+//     "downloads_today": 0,
+//     "downloads_limit": 10,
+//     "confirmed": 1,
+//     "isPremium": 0,
+//     "personalDomains": [
+//         "lib-vyfxb4urfpft3qi5b43ewpgh.fit.wf",
+//         "lib-bxtnqoxeg7dxe37xluhzbydh.yeah.pw"
+//     ]
+// }
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    id: i32,
+    email: String,
+    name: String,
+    kindle_email: String,
+    remix_userkey: String,
+    donations_activate: Option<String>,
+    donations_expire: Option<String>,
+    downloads_today: i32,
+    downloads_limit: i32,
+    confirmed: i32,
+    isPremium: i32,
+    personalDomains: Vec<String>
+}
+
+// example:
+// {
+//     "id": 438227,
+//     "title": "Teach Yourself Electricity and Electronics",
+//     "author": "Stan Gibilisco",
+//     "cover": "https://static.singlelogin.re/covers299/books/eb/16/85/eb1685e7ec252888c2855a9646884f9b.jpg",
+//     "hash": "c44dd4"
+// }
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Book {
+    id: i32,
+    title: String,
+    author: String,
+    cover: String, // url
+    hash: String
+}
+
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct User {}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Book {}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Info {}
+pub struct Info {
+    books:i32,
+    articles: i32,
+    plans: Value,
+    languages: HashMap<String,String>,
+    extensions: Vec<String>,
+    banner:String,
+    service: HashMap<String,String>,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Format {}
+
+// I don't know if is nesseary
+//
+// #[derive(Debug, Serialize, Deserialize)]
+// pub struct ZWapper<T> {
+//     success: i32,
+//     body: T
+// }
