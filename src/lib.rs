8433288e9cf61aa7e33b569a494c00dc3a4f8784
Lird <839122419@qq.com>
2023-06-14 22:58:32 +0800

    update dep and add some api

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
-extern crate serde_json;
+use serde::{Deserialize,Serialize};
 
 use z_api_def::{Api, Post, Get};
 use serde_json::{Result, Value};
 
+#[derive(Debug,Serialize, Deserialize)]
+pub struct Book {
+
+}
 
 /// The api definitions.
 #[Api]
 pub trait  ZApi{
-    #[Post("/eapi/user/login")]
-    fn login(&self, email: String, password: String) -> Result<Value>;
+    // #[Post("/eapi/user/login")]
+    // fn login(&self, email: String, password: String) -> Result<Value>;
 
     #[Get("/eapi/user/profile")]
     fn get_profile(&self) -> Result<Value>;
 
+    #[Get("/eapi/book/recently")]
+    fn get_book_recently(&self) -> Result<Vec<Book>>;
 }
