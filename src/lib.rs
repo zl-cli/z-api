6cd1de1ce99611d06535aef438a4e751913d8e6b
Lird <839122419@qq.com>
2023-06-15 14:16:37 +0800

    update switch language param

--- src/lib.rs
+++ src/lib.rs
@@ -30,14 +30,14 @@ pub trait ZApi {
     #[Get("/eapi/book/recently")]
     fn get_book_recently(&self) -> Result<Vec<Book>>;
 
-    #[Get("/eapi/book/most-popular")]
-    fn get_book_most_popular(&self /* switch_language: String */) -> Result<Vec<Book>>;
+    #[Get("/eapi/book/most-popular?switch_language={switch_language}")]
+    fn get_book_most_popular(&self, switch_language: String) -> Result<Vec<Book>>;
 
     #[Get("/eapi/book/{id}/{hash}/send-to-{device}")]
     fn send_book_to_device(&self, id: i32, hash: i32, device: String) -> Result<()>;
 
-    #[Get("/eapi/book/{id}/{hash}")]
-    fn get_book(&self, id: i32, hash: i32 /* switch_language: String */) -> Result<Book>;
+    #[Get("/eapi/book/{id}/{hash}?switch_language={switch_language}")]
+    fn get_book(&self, id: i32, hash: i32, switch_language:String) -> Result<Book>;
 
     #[Get("/eapi/book/{id}/{hash}/similar")]
     fn get_similar_books(&self, id: i32, hash: i32) -> Result<Vec<Book>>;
